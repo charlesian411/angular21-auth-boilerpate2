5f26d444adcbed418e4571ac642b9d0d9128d571
Charles Ian <charlesian@Mac.bbrouter>
2026-05-11 02:08:20 +0800

    Optimize SMTP for port 2525 and ensure environment variables override config

--- backend/_helpers/send-email.ts
+++ backend/_helpers/send-email.ts
@@ -21,13 +21,16 @@ if (process.env.SENDGRID_API_KEY) {
     };
 } else {
     // USE ETHEREAL OR OTHER SMTP
-    config.smtpOptions = config.smtpOptions || {
+    config.smtpOptions = {
         host: process.env.SMTP_HOST,
         port: Number(process.env.SMTP_PORT) || 587,
         secure: Number(process.env.SMTP_PORT) === 465,
         auth: {
             user: process.env.SMTP_USER,
             pass: process.env.SMTP_PASS
+        },
+        tls: {
+            rejectUnauthorized: Number(process.env.SMTP_PORT) !== 2525
         }
     };
 }
