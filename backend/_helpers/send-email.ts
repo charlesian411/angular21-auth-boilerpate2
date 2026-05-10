1c1e3a4eb17a0062c7d3d3756f7aa36c150d8c60
Charles Ian <charlesian@Charless-MacBook-Air.local>
2026-05-10 13:38:34 +0800

    Support SSL for email on port 465

--- backend/_helpers/send-email.ts
+++ backend/_helpers/send-email.ts
@@ -7,7 +7,8 @@ try {
         emailFrom: process.env.EMAIL_FROM,
         smtpOptions: {
             host: process.env.SMTP_HOST,
-            port: process.env.SMTP_PORT,
+            port: Number(process.env.SMTP_PORT) || 587,
+            secure: Number(process.env.SMTP_PORT) === 465,
             auth: {
                 user: process.env.SMTP_USER,
                 pass: process.env.SMTP_PASS
